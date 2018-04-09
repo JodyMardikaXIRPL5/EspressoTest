dc91370920e296e8cf1cb56b97d8659234bbddd0
Jody Mardika <jody_mardika_24rpl@student.smktelkom-mlg.sch.id>
2018-04-09 10:50:51 +0700

    test run

---
+++ app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
@@ -0,0 +1,40 @@
+package com.example.android.teatime.IdlingResource;
+
+import android.support.annotation.Nullable;
+import android.support.test.espresso.IdlingResource;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by OCHA on 28/01/2018.
+ */
+
+public class SimpleIdlingResource implements IdlingResource {
+    @Nullable
+    private volatile ResourceCallback mCallback;
+
+    // Idleness is controlled with this boolean.
+    private AtomicBoolean mIsIdleNow = new AtomicBoolean(true);
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        return mIsIdleNow.get();
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback callback) {
+        mCallback = callback;
+    }
+
+    public void setIdleState(boolean isIdelNow) {
+        mIsIdleNow.set(isIdelNow);
+        if (isIdelNow && mCallback != null) {
+            mCallback.onTransitionToIdle();
+        }
+    }
+}
